5c66b90c914de226fd9bd56636bc254f450d5635
evert <evert@everbuild.be>
2021-08-22 16:21:49 +0200

    not needed anymore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import {resolve} from "path";
-
-const globalScss = resolve(__dirname, "./src/assets/sass/global");
 
 // https://vitejs.dev/config/
 export default defineConfig({
